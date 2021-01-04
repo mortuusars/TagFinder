c98d93e190f358e5ed77305f2f139343bf688084
Evgeny Daubel <mortuusars@gmail.com>
2021-01-04 21:29:03 +0200

    Selected count now highlights in red when count is larger than 30.

---
+++ TagFinder/ViewModels/Converters/CountForegroundConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TagFinder.ViewModels.Converters
+{
+    [ValueConversion(typeof(int), typeof(SolidColorBrush))]
+    public class CountForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return new SolidColorBrush((Color) ColorConverter.ConvertFromString("#ff6666"));
+            else if ((int)value > 30)
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ff6666"));
+            else
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("Gray"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- TagFinder/ViewModels/TagsViewModel.cs
+++ TagFinder/ViewModels/TagsViewModel.cs
@@ -27,7 +27,7 @@ namespace TagFinder.ViewModels
         public bool IncludeGlobalCount { get; set; } = false;
 
         public string Status { get; set; } = StatusManager.Status;
-        public string SelectedCount { get; set; }
+        public int SelectedCount { get; set; }
 
         public bool IsUserInfoAvailable { get; set; }
         public string LoggedUsername { get; set; }
@@ -82,7 +82,7 @@ namespace TagFinder.ViewModels
 
         private void OnSelectedChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
-            SelectedCount = SelectedTagsList.Count > 0 ? SelectedTagsList.Count.ToString() : "";
+            SelectedCount = SelectedTagsList.Count > 0 ? SelectedTagsList.Count : 0;
         }
 
         private async void OnLogOutCommand()
